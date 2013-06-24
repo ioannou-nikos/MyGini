5d2b5c3cf551cd791c0b83938cc2b23e1c824680
Nikos Ioannou <ioannou.nikos@gmail.com>
2013-06-25 02:13:24 +0300

    PhoneReader and db.properties

--- src/mygini/model/readers/PhoneReader.java
+++ src/mygini/model/readers/PhoneReader.java
@@ -5,7 +5,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 import mygini.model.Phone;
 
 /**
@@ -13,9 +12,9 @@ import mygini.model.Phone;
  * @author urbanspieces
  */
 public class PhoneReader {
-    public static List<Phone> readPersonPhones(Connection c, long person_id)
+    public static ArrayList<Phone> readPersonPhones(Connection c, long person_id)
             throws SQLException{
-        List<Phone> phones = new ArrayList<>();
+        ArrayList<Phone> phones = new ArrayList<>();
         Statement st = c.createStatement();
         String sql = "SELECT * FROM person_phones WHERE person_id = " + person_id;
         ResultSet rs = st.executeQuery(sql);
@@ -23,6 +22,20 @@ public class PhoneReader {
             Phone p = new Phone(rs.getLong("phone_id"), rs.getLong("person_id"),
                     rs.getString("phone"),rs.getString("type"));
             p.validate();
+            phones.add(p);
+        }
+        return phones;
+    }
+    
+    public static ArrayList<Phone> readAllPhones(Connection c) throws SQLException{
+        ArrayList<Phone> phones = new ArrayList<>();
+        Statement st = c.createStatement();
+        String sql = "SELECT * FROM person_phones";
+        ResultSet rs = st.executeQuery(sql);
+        while (rs.next()){
+            Phone p = new Phone(rs.getLong("phone_id"), rs.getLong("person_id"),
+                    rs.getString("phone"),rs.getString("type"));
+            //p.validate();
             phones.add(p);
         }
         return phones;
